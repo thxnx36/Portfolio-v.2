b1cfbaf3fc7d291d4718dcca2ca86e171525df7a
Vlad K <wlad3430@gmail.com>
2024-05-19 02:25:46 +0300

    fixed reCaptcha key

--- src/shared/re-captcha/ReCaptcha.tsx
+++ src/shared/re-captcha/ReCaptcha.tsx
@@ -8,12 +8,14 @@ type Props = {
 
 export const ReCaptcha: FC<Props> = ({ onChange }) => {
   const { theme } = useContext(ThemeContext)
+  const key = '6LeHRuEpAAAAAN85i4oh3n-q-xvObU4mK_N8t-eG'
+  // import.meta.env.VITE_CAPTCHA_SITE_KEY
 
   return (
     <ReCAPTCHA
       //@ts-ignore
       theme={theme}
-      sitekey={import.meta.env.VITE_CAPTCHA_SITE_KEY}
+      sitekey={key}
       onChange={onChange}
     />
   )
