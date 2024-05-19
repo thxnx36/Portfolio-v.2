e0c808eff7a567ecbfda69c882a31c69dde413d2
Vlad K <wlad3430@gmail.com>
2024-05-19 19:29:46 +0300

    Fixed url

--- src/db/education.ts
+++ src/db/education.ts
@@ -21,13 +21,4 @@ export const education = [
     description:
       'Learning of energy supply plans and strategies, energy optimization and cost minimization',
   },
-  /* {
-    id: 3,
-    university: "Metallurgical College of Zaporizhzhia",
-    position: "Student",
-    date: "Sep 2011 - May 2015",
-    title: "Installation and operation of electrical equipment",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui.",
-  }, */
 ]
