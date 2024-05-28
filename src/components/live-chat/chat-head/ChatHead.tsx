f55aa9ca3410f8ad979996ce2c0c10d475237d7e
Vlad K <wlad3430@gmail.com>
2024-05-28 20:46:46 +0300

    fixed styles

--- src/components/live-chat/chat-head/ChatHead.tsx
+++ src/components/live-chat/chat-head/ChatHead.tsx
@@ -1,36 +1,26 @@
 import { FC } from 'react'
 import { Avatar, Paragraph } from '../../../shared'
 import { IoClose } from 'react-icons/io5'
-import { IoChatbubbles } from 'react-icons/io5'
 import { chatPhoto } from '../../../assets'
 import styles from './ChatHead.module.css'
 
 type Props = {
   onToggleChat: () => void
-  isOpenChat: boolean
 }
 
-export const ChatHead: FC<Props> = ({ onToggleChat, isOpenChat }) => {
+export const ChatHead: FC<Props> = ({ onToggleChat }) => {
   return (
-    <div className={styles.buttonsContainer}>
-      {isOpenChat ? (
-        <>
-          <div className={styles.contactWrap}>
-            <Avatar className={styles.avatar} src={chatPhoto} />
-            <div className={styles.contact}>
-              <Paragraph>Vlad</Paragraph>
-              <Paragraph className={styles.status}>online</Paragraph>
-            </div>
-          </div>
-          <button className={styles.closeButton} onClick={onToggleChat}>
-            <IoClose size={'2em'} />
-          </button>
-        </>
-      ) : (
-        <button className={styles.openButton} onClick={onToggleChat}>
-          Live Chat <IoChatbubbles size={'1.2em'} />
-        </button>
-      )}
+    <div className={styles.chatHeadContainer}>
+      <div className={styles.contactWrap}>
+        <Avatar className={styles.avatar} src={chatPhoto} />
+        <div className={styles.contact}>
+          <Paragraph>Vlad</Paragraph>
+          <Paragraph className={styles.status}>online</Paragraph>
+        </div>
+      </div>
+      <button className={styles.closeButton} onClick={onToggleChat}>
+        <IoClose size={'2em'} />
+      </button>
     </div>
   )
 }
