d6c6a3cce50c416bfcfff6a2ce888d22a2492269
Vlad K <wlad3430@gmail.com>
2024-07-19 22:08:04 +0300

    Updated  useAnimateDuringScroll hook & TransitionContainer component

--- src/shared/transition-container/TransitionContainer.tsx
+++ src/shared/transition-container/TransitionContainer.tsx
@@ -1,18 +1,49 @@
-import { FC, ReactNode } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
+import { FC, ReactNode, useRef } from 'react'
+import { motion, AnimatePresence, useInView } from 'framer-motion'
 
 type Props = {
   children: ReactNode
+  className?: string
+  variant?: {
+    hidden: { y: number }
+    visible: { y: number }
+  }
+  duration?: number
+  delay?: number
+  yOffset?: number
+  blur?: string
 }
 
-export const TransitionContainer: FC<Props> = ({ children }) => {
+export const TransitionContainer: FC<Props> = ({
+  children,
+  variant,
+  duration = 0.4,
+  delay = 0,
+  yOffset = 6,
+  blur = '5px',
+}) => {
+  const ref = useRef<HTMLDivElement | null>(null)
+  const inViewResult = useInView(ref, { once: true })
+
+  const defaultVariants = {
+    hidden: { y: yOffset, opacity: 0, filter: `blur(${blur})` },
+    visible: { y: -yOffset, opacity: 1, filter: `blur(0px)` },
+  }
+  const combinedVariants = variant || defaultVariants
+
   return (
     <AnimatePresence>
       <motion.div
-        initial={{ translate: '-100%' }}
-        animate={{ translate: '0%' }}
-        exit={{ translate: '100%' }}
-        transition={{ duration: 0.9, ease: [0.22, 1, 0.36, 1] }}
+        ref={ref}
+        initial='hidden'
+        animate={inViewResult ? 'visible' : 'hidden'}
+        exit='hidden'
+        variants={combinedVariants}
+        transition={{
+          delay: 0.04 + delay,
+          duration,
+          ease: 'easeOut',
+        }}
       >
         {children}
       </motion.div>
